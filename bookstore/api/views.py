6f5bbaa7f315c4b58e6a6c07966e5f062ee6bf7f
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-26 09:13:58 +0700

    set up templates, static home page

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def books(request):
-    return HttpResponse("Book Page")
+    context = {}
+    return render(request,"home/index.html",context)
