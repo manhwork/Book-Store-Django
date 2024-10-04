553f61a80bce8ee797326276069417a04fab48c6
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-10-04 08:58:28 +0700

    Sửa model

--- bookstore/api/admin.py
+++ bookstore/api/admin.py
@@ -4,23 +4,23 @@ from .models import Product, Category
 
 # Register your models here.
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('image_tag', 'title', 'created_at', 'updated_at','slug')
-    search_fields = ('title', 'description')
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ["title"]
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('image_tag', 'title', 'created_at', 'updated_at','slug')
+    # search_fields = ('title', 'description')
+    # prepopulated_fields = {'slug': ('title',)}
+    # list_filter = ["title"]
 
-    def image_tag(self, obj):
-        if obj.image:
-            return format_html('<img src="./media/category/Capturehiu.PNG" width="50" height="50" />')
-        return '-'
-    image_tag.short_description = 'Image'
+    # def image_tag(self, obj):
+    #     if obj.image:
+    #         return format_html('<img src="./media/category/Capturehiu.PNG" width="50" height="50" />')
+    #     return '-'
+    # image_tag.short_description = 'Image'
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'discount', 'category', 'quantity', 'created_at', 'updated_at','slug')
-    search_fields = ('title', 'description')
-    list_filter = ('category','title','price')
-    prepopulated_fields = {'slug': ('title',)}
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'price', 'discount', 'quantity', 'created_at', 'updated_at','slug')
+#     search_fields = ('title', 'description')
+#     list_filter = ('title','price')
+#     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Category, CategoryAdmin)
--- bookstore/api/models.py
+++ bookstore/api/models.py
@@ -5,8 +5,10 @@ from django.utils.html import mark_safe
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='./media/category', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='./static/media/category', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=255, default="active")
+    deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True, blank=True)
@@ -20,24 +22,24 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-# Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='./media/product', null= False)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(unique=True, blank=True)
+    price = models.FloatField()
+    discountPercentage = models.FloatField()
+    stock = models.IntegerField()
+    thumbnail = models.ImageField(upload_to='./static/media/category', blank=True, null=True)
+    category_id = models.CharField(max_length=255)
+    awsome_product = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default="active")
+    deleted = models.BooleanField(default=False)
+    reviews = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
