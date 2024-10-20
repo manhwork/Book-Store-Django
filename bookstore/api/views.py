f9da2dca7100ad117e13e5087b7c610769f88b13
Akimboo <149180748+nmpogg@users.noreply.github.com>
2024-10-20 21:12:30 +0700

    push branch

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -1,16 +1,21 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
+    
     return render(request,"home/index.html",{
         "title" : "Home",
     })
 
 def books(request):
-    return render(request,"books/index.html",{
+    l=Book.objects.all()
+    print(l)
+    return render(request,"books/index.html",context={
         "title" : "Books",
+        "books":l
     })
 
 def author_about(request):
